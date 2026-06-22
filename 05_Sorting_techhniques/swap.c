35f334dd2f3f723819687dc2339ea23b024a96f2
SovanAI <pwangdu323@gmail.com>
2026-06-22 22:50:15 +0530

    The switch section is added with a third variable successfully

--- 05_Sorting_techhniques/swap.c
+++ 05_Sorting_techhniques/swap.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
+void swap_using_var(int a , int b)
+{
+    printf("%d\n", a);
+    printf("%d \n", b);   // swaping the value using the third variable 
+    int temp ;
+    temp = a;
+    a = b ;
+    b = temp;
+    printf("Swap successfully done\n");
+    printf("%d \n", a);
+    printf("%d", b);
+}
 
 void swap(int a , int b)
-{  printf("%d\n", a );
-    printf("%d\n", b);
+{  
+    printf("%d\n", a );
+    printf("%d\n", b);   // with out using a third variable
     a = a + b;
     b = a - b ; 
     a = a - b; 
